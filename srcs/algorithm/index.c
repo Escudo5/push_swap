044be7b5d1f0b753f2dc33b170d5947449a1444b
Escudo5 <sergiomargar03@gmail.com>
2024-11-26 11:18:50 +0100

    index y inicio logica

---
+++ srcs/algorithm/index.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 10:35:26 by smarquez          #+#    #+#             */
+/*   Updated: 2024/11/26 11:05:47 by smarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	index(t_stack_node **a)
+{
+	t_stack_node	*smallest_node;
+	int				current_index;
+
+	current_index = 0;
+	while (unindexed(*a))
+	{
+		smallest_node = find_smallest_unindexed(*a);
+		smallest_node->index = current_index;
+		current_index++;
+	}
+}
+
+bool	unindexed(t_stack_node *a)
+{
+	while (a)
+	{
+		if (a->index == -1)
+			return (true);
+		a = a->next;
+	}
+	return (false);
+}
+
+t_stack_node	*find_smallest_unindexed(t_stack_node *a)
+{
+	t_stack_node *smallest_node;
+
+	smallest_node = NULL;
+	while (a)
+	{
+		if (a->index == -1 && (!smallest_node || a->nbr < smallest_node->nbr))
+			smallest_node = a;
+		a = a->next;
+	}
+	return (smallest_node);
+}
--- srcs/algorithm/logic.c
+++ srcs/algorithm/logic.c
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pa.c                                               :+:      :+:    :+:   */
+/*   logic.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/25 16:09:30 by smarquez          #+#    #+#             */
-/*   Updated: 2024/11/26 10:31:08 by smarquez         ###   ########.fr       */
+/*   Created: 2024/11/26 11:16:03 by smarquez          #+#    #+#             */
+/*   Updated: 2024/11/26 11:16:04 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-void	pa(t_stack_node **a, t_stack_node **b)
-{
-	t_stack_node	*node;
-
-	if (*b == NULL)
-		return ;
-	node = *b;
-	*b = (*b)->next;
-	if (*b)
-		(*b)->prev = NULL;
-	node->next = *a;
-	if (*a)
-		(*a)->prev = node;
-	*a = node;
-	node->prev = NULL;
-}
